7a5f956ab10503d44a5fe2eabf3b9cce44a7e2cd
ilyas-homedev <ilyasov000fp@gmail.com>
2021-09-29 23:17:36 +0200

    add result item template

--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  settingsOpened: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openSettings() {
+    this.settingsOpened = !this.settingsOpened;
   }
 
 }
